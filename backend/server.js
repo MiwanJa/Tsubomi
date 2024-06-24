ffe4ce9628880f89cf66d07e7b7115bd91e2e22a
Miwanja <misha787981@gmail.com>
2024-06-24 22:42:53 +0300

    upDate -> server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db")
+const userRoutes = require("./models/user")
+const authRoutes = require("./models/auth")
 
 //database connection
 connection()
@@ -11,6 +13,11 @@ connection()
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+//
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`))
